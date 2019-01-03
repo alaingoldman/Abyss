81f607edeaee718f3684fba16ae22a439bac3702
Alain Godman <alain.goldman@gmail.com>
2019-01-03 07:15:23 -0500

    initial

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router'; 
+
+class AboutPage extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+                <p> This app uses react... blah blah.</p>
+            </div>
+        );
+    }
+}
+
+export default AboutPage;
---
+++ src/index.test.js
@@ -0,0 +1,18 @@
+import expect from 'expect';
+import { JSDOM } from 'jsdom';
+import fs from 'fs';
+
+describe('Our first test', () => {
+  it('should pass', () => {
+    expect(true).toEqual(true);
+  });
+}); 
+
+describe('index.html', () => {
+  it('should say hello', () => {
+    const index = fs.readFileSync('./src/index.html');
+    const dom = new JSDOM(index);
+    const contents = dom.window.document.querySelector("h1").textContent;
+    expect(contents, 'Hello World!');
+  });
+});
