cbb29eb99c1d910faf422650a5491ec2354e31c3
Alain Godman <alain.goldman@gmail.com>
2019-01-03 07:18:09 -0500

    removed curses from the code

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -31,50 +31,6 @@ require.extensions['.jpg'] = function () {return null;};
 
 // Configure JSDOM and set global variables
 // to simulate a browser environment for tests.
-// var jsdom = require('jsdom').jsdom;
-
-// var exposedProperties = ['window', 'navigator', 'document'];
-
-// global.document = jsdom('');
-// global.window = document.defaultView;
-// Object.keys(document.defaultView).forEach((property) => {
-//   if (typeof global[property] === 'undefined') {
-//     exposedProperties.push(property);
-//     global[property] = document.defaultView[property];
-//   }
-// });
-
-// global.navigator = {
-//   userAgent: 'node.js'
-// };
-
-// documentRef = document;  //eslint-disable-line no-undef
-// var jsdom = require('jsdom');
-// const { jsdom } = require('jsdom');
-// const { document } = (new jsdom('')).window;
-// global.document = document;
-// global.document = jsdom('');
-// global.window = document.defaultView;
-// global.navigator = {
-//   userAgent: 'node.js'
-// };
-
-// function copyProps(src, target) {
-//   const props = Object.getOwnPropertyNames(src)
-//     .filter(prop => typeof target[prop] === 'undefined')
-//     .reduce((result, prop) => ({
-//       ...result,
-//       [prop]: Object.getOwnPropertyDescriptor(src, prop),
-//     }), {});
-//   Object.defineProperties(target, props);
-// }
-// copyProps(document.defaultView, global);
-
-
-
-
-
-
 
 const { JSDOM } = require('jsdom');
 const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
