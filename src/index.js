65c25a2d37f00c977a82cc857b94439a20376a4b
Alain Godman <alain.goldman@gmail.com>
2019-01-04 09:54:57 -0500

    added faux api

--- src/index.js
+++ src/index.js
@@ -2,11 +2,22 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { getUsers } from './api/usersApi';
 import routes from './routes';
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-render(
-    <Router history={browserHistory} routes={routes} />,
-    document.getElementById('app')
-);
+getUsers().then(result => {
+    let usersBody = "";
+
+    result.forEach(user => {
+        usersBody+= `<tr><td>${user.id}</td><td>${user.firstName}</td><td>${user.lastName}</td><td>${user.email}</td></tr>`;
+    });
+
+    global.document.getElementById('users').innerHTML = usersBody;
+
+});
+// render(
+//     <Router history={browserHistory} routes={routes} />,
+//     document.getElementById('app')
+// );
