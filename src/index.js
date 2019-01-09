7471fb649ea7f9f557ec53b45f30149a1b2ae6f7
Alain Godman <alain.goldman@gmail.com>
2019-01-09 12:56:46 -0500

    updated fake api with some options

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ getUsers().then(result => {
     let usersBody = "";
 
     result.forEach(user => {
-        usersBody+= `<tr><td>${user.id}</td><td>${user.firstName}</td><td>${user.lastName}</td><td>${user.email}</td></tr>`;
+        usersBody+= `<tr><td><b>${user.id} &nbsp;</b></td><td>${user.firstName}</td><td>${user.lastName}</td><td>${user.email}</td></tr>`;
     });
 
     global.document.getElementById('users').innerHTML = usersBody;
