e2424971ad6102e7e86f68c295e1fe506bca5940
Alain Godman <alain.goldman@gmail.com>
2019-01-12 10:29:14 -0500

    used a real api

--- tools/distServer.js
+++ tools/distServer.js
@@ -12,15 +12,6 @@ app.use(compression()); // this is NOT for actual production use. This is just u
                         // minified production build for local debugging purposes.
 app.use(express.static('dist'));
 
-app.get('/users', function(req, res) {
-  // Hard coding for simplicity, Pretend this hits a real database
-  res.json([
-    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
-    {"id": 1, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com"},
-    {"id": 1, "firstName": "Tina", "lastName": "lee", "email": "lee.tina@hotmail.com"}
-  ]);
-});
-
 app.get('*', function(req, res) {
   res.sendFile(path.join( __dirname, '../dist/index.html'));
 });
