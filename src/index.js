38bfdeda1a73d1ff5fb2f1a3ac5f113241654fbc
Alain Godman <alain.goldman@gmail.com>
2019-01-14 11:12:28 -0500

    brought react back

--- src/index.js
+++ src/index.js
@@ -1,41 +1,13 @@
 import './styles/styles.css'; //Webpack can import CSS files too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'babel-polyfill';
-import { deleteUser, getUsers } from './api/usersApi';
 
-// import React from 'react';
-// import routes from './routes';
-// import { render } from 'react-dom';
-// import { browserHistory, Router } from 'react-router';
+import { Router, browserHistory } from 'react-router';
+import React from 'react';
+import { render } from 'react-dom';
+import routes from './routes';
 
-getUsers().then(result => {
-    let usersBody = "";
-
-    result.forEach(user => {
-        usersBody+= `<tr>
-                <td><a href="#" data-id="${user.id}" class="deleteUser">Delete</a></td>
-                <td><b>${user.id}</b></td>
-                <td>${user.firstName}</td>
-                <td>${user.lastName}</td>
-                <td>${user.email}</td>
-            </tr>`;
-    });
-
-    global.document.getElementById('users').innerHTML = usersBody;
-
-    const deleteLinks = global.document.getElementsByClassName('deleteUser');
-    Array.from(deleteLinks, link => {
-        link.onclick = function(event) {
-            const element = event.target; 
-            event.preventDefault(); 
-            deleteUser(element.attributes["data-id"].value);
-            const row = element.parentNode.parentNode; 
-            row.parentNode.removeChild(row);
-        };
-    });
-});
-
-// render(
-//     <Router history={browserHistory} routes={routes} />,
-//     document.getElementById('app')
-// );
+render(
+    <Router history={browserHistory} routes={routes} />,
+    document.getElementById('app')
+);
