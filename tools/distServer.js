b56c9b5dd7e18a4926e8dd71a8959483c6a1cc05
Alain Godman <alain.goldman@gmail.com>
2019-01-16 12:51:34 -0500

    wip

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,25 +1,25 @@
-import compression from 'compression';
+// import compression from 'compression';
 import express from 'express';
-import open from 'open';
-import path from 'path';
+// import open from 'open';
+// import path from 'path';
 
 /* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
 
-app.use(compression()); // this is NOT for actual production use. This is just useful for hosting the 
+// app.use(compression()); // this is NOT for actual production use. This is just useful for hosting the 
                         // minified production build for local debugging purposes.
-app.use(express.static('dist'));
+// app.use(express.static('dist'));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join( __dirname, '../dist/index.html'));
-});
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join( __dirname, '../dist/index.html'));
+// });
 
-app.listen(port, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    open(`http://localhost:${port}`);
-  }
-});
+// app.listen(port, function(err) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     open(`http://localhost:${port}`);
+//   }
+// });
