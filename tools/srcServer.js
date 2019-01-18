a1ba451e9dac409302f3cef2bd7e9d2df4a18374
Alain Godman <alain.goldman@gmail.com>
2019-01-17 22:53:50 -0500

    install heroku remove distServer

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -1,44 +1,24 @@
-import config from '../webpack.config.dev';
-import express from 'express';
-import open from 'open';
-import path from 'path';
-import webpack from 'webpack';
+var express =  require('express');
+var cors = require('cors');
 
-/* eslint-disable no-console */
-
-const port = 3000;
 const app = express();
-const compiler = webpack(config);
+app.use(cors());
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
+app.set('port', (process.env.PORT || 5000));
 
-app.use(require('webpack-hot-middleware')(compiler));
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+});
 
 app.get('/users', function(req, res) {
-  // Hard coding for simplicity, Pretend this hits a real database
+  // Hard coding for simplicity. Pretend this hits a real database
   res.json([
-    {"id": 1, "firstName": "Bob", "lastName": "Smith", "email": "bob@gmail.com"},
-    {"id": 1, "firstName": "Tammy", "lastName": "Norton", "email": "tnorton@yahoo.com"},
-    {"id": 1, "firstName": "Tina", "lastName": "lee", "email": "lee.tina@hotmail.com"}
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"firstName":"Tammy","lastName":"Norton","email":"tnorton@yahoo.com"},
+    {"id": 3,"firstName":"Tina","lastName":"Lee","email":"lee.tina@hotmail.com"}
   ]);
 });
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join( __dirname, '../src/index.html'));
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port')); 
 });
-
-
-app.listen(port, function(err) {
-  if (err) {
-    console.log(err);
-  } else {
-    open(`http://localhost:${port}`);
-  }
-});
-
-// app.listen(app.get('port'), function() {
-//   console.log("Node app is running at localhost:" + app.get('port')); 
-// });
